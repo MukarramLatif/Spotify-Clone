79bed1fa5bb8d9ac89f2f3c9c27591cbc35e7253
MukarramLatif <133117486+MukarramLatif@users.noreply.github.com>
2023-08-09 20:56:58 +0500

    Add files via upload

---
+++ NCS/NCS.js
@@ -0,0 +1,144 @@
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio("songs/1.mp3");
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+let masterSongName = document.getElementById("masterSongName");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
+
+let songs = [
+  {
+    songName: " Heroes Tonight- NCS Release",
+    filePath: "songs/1.mp3",
+    coverPath: "NCS cover/1.jpg",
+  },
+  {
+    songName: "Heart Afire - NCS Release",
+    filePath: "songs/2.mp3",
+    coverPath: "NCS cover/2.jpg",
+  },
+  {
+    songName: "Adventure - NCS Release",
+    filePath: "songs/3.mp3",
+    coverPath: "NCS cover/3.jpg",
+  },
+  {
+    songName: "Symbolism - NCS Release",
+    filePath: "songs/4.mp3",
+    coverPath: "NCS cover/4.jpg",
+  },
+  {
+    songName: "Jumbo - NCS Release",
+    filePath: "songs/5.mp3",
+    coverPath: "NCS cover/5.jpg",
+  },
+  {
+    songName: "Be Together - NCS Release",
+    filePath: "songs/6.mp3",
+    coverPath: "NCS cover/6.jpg",
+  },
+  {
+    songName: "Make Me Move - NCS Release",
+    filePath: "songs/7.mp3",
+    coverPath: "NCS cover/7.jpg",
+  },
+  {
+    songName: "Mortals - NCS Release",
+    filePath: "songs/8.mp3",
+    coverPath: "NCS cover/8.jpg",
+  },
+  {
+    songName: "Flares - NCS Release",
+    filePath: "songs/9.mp3",
+    coverPath: "NCS cover/9.jpg",
+  },
+];
+
+songItems.forEach((element, i) => {
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
+
+// Handle play/pause click
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-pause-circle");
+    masterPlay.classList.add("fa-play-circle");
+    gif.style.opacity = 0;
+  }
+});
+// Listen to Events
+audioElement.addEventListener("timeupdate", () => {
+  // Update Seekbar
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+  myProgressBar.value = progress;
+});
+
+myProgressBar.addEventListener("change", () => {
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
+});
+
+const makeAllPlays = () => {
+  Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+    (element) => {
+      element.classList.remove("fa-pause-circle");
+      element.classList.add("fa-play-circle");
+    }
+  );
+};
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+  (element) => {
+    element.addEventListener("click", (e) => {
+      makeAllPlays();
+      songIndex = parseInt(e.target.id);
+      e.target.classList.remove("fa-play-circle");
+      e.target.classList.add("fa-pause-circle");
+      audioElement.src = `songs/${songIndex + 1}.mp3`;
+      masterSongName.innerText = songs[songIndex].songName;
+      audioElement.currentTime = 0;
+      audioElement.play();
+      gif.style.opacity = 1;
+      masterPlay.classList.remove("fa-play-circle");
+      masterPlay.classList.add("fa-pause-circle");
+    });
+  }
+);
+
+document.getElementById("next").addEventListener("click", () => {
+  if (songIndex >= 9) {
+    songIndex = 0;
+  } else {
+    songIndex += 1;
+  }
+  audioElement.src = `songs/${songIndex + 1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove("fa-play-circle");
+  masterPlay.classList.add("fa-pause-circle");
+});
+
+document.getElementById("previous").addEventListener("click", () => {
+  if (songIndex <= 0) {
+    songIndex = 0;
+  } else {
+    songIndex -= 1;
+  }
+  audioElement.src = `songs/${songIndex + 1}.mp3`;
+  masterSongName.innerText = songs[songIndex].songName;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove("fa-play-circle");
+  masterPlay.classList.add("fa-pause-circle");
+});
